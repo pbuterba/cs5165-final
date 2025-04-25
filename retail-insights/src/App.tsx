973afd05ba093408c7f86cbf6f60462d27b0a0c0
bellettichloe <123496194+bellettichloe@users.noreply.github.com>
2025-04-25 16:12:10 -0400

    Update App.tsx

--- retail-insights/src/App.tsx
+++ retail-insights/src/App.tsx
@@ -62,15 +62,4 @@ const App: React.FC = () => {
 };
 
 export default App;
-=======
-import './App.css'
 
-function App() {
-  return (
-    <div>
-      <h1 id="placeholder-title">App Landing Page</h1>
-    </div>
-  )
-}
-
-export default App
